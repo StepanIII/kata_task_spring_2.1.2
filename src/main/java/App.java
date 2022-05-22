6ba95d4936d63e440009750daac3e8da89a9d1f0
Stepan Cupriyanovich <stepancupriyanovich@gmail.com>
2022-05-22 15:35:23 +0300

    Create a cat bean and compare beans

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanHelloWorld2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat cat1 = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+
+        System.out.println("First bean helloworld = second bean helloworld: " + (bean == beanHelloWorld2));
+        System.out.println("First bean cat = second bean cat: " + (cat1 == cat2));
     }
 }
